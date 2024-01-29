5865094b8165443f768afd4e6a23b84aaf5a544d
Kaif-Busy <kaif@mail.busy.in>
2024-01-29 15:20:20 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	_ "math"
+	"reflect"
+)
+
+
+
+
+
+func setKeyValue(key string, value interface{}, source map[string]interface{}) bool {
+
+	var ans bool
+	if _, ok := source[key]; ok {
+		source[key] = value
+		return true
+	} else {
+		for _, y := range source {
+
+			if reflect.TypeOf(y).Kind() == reflect.Map {
+				ans = setKeyValue(key, value, y.(map[string]interface{}))
+
+			}
+
+		}
+	}
+	return ans
+}
+
+
+
+func main() {
+
+	foods := map[string]interface{}{
+		"Food": "delicious",
+		"eggs": map[string]interface{}{
+			"greeting": "Hello",
+			"name":     "Kaif",
+			"course":   "MCA",
+		},
+		"name":   "Soransh",
+		"course": "BTech",
+	}
+
+	
+
+	x := setKeyValue("Food", "Very Delicious", foods)
+	
+	if x {
+		fmt.Println("Changed")
+	} else {
+		fmt.Println("Not Found")
+	}
+	
+
+	fmt.Println(foods)
+
+	
+
+}
